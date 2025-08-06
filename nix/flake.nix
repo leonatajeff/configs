b2408e67075b76b7cbdf31e4aa3da6e559a5b615
jeff.leonata <jeff.leonata@pnnl.gov>
2025-08-06 17:12:26 -0400

    update flake

--- nix/flake.nix
+++ nix/flake.nix
@@ -2,49 +2,80 @@
   description = "bo nix";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nix-darwin.url = "github:nix-darwin/nix-darwin/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-24.11";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
   let
-    configuration = { pkgs, ... }: {
+    system = "aarch64-darwin";
+    configuration = { pkgs, config, ... }: {
+      nixpkgs.config.allowUnfree = true;
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
       environment.systemPackages = with pkgs; [ 
-	vim
-	neovim
-	git
-	curl
-	tmux
-	mkalias
-	nix-zsh-completions
-	nodejs_20
-	yarn
-	tree
-	prettierd
-	postgresql
-	corepack
-	aerospace
-	ffmpeg
-	kubectl
-	docker
-	awscli2
+	 alacritty
+	 lazygit
+	 neovim
+	 git
+	 curl
+	 tmux
+	 mkalias
+	 nix-zsh-completions
+	 nodejs_20
+	 vscode
+	 yarn
+	 tree
+	 prettierd
+	 postgresql
+	 postgresql16Packages.postgis
+	 corepack
+	 aerospace
+	 ffmpeg
+	 kubectl
+	 docker
+	 awscli2
       ];
+
+      system.activationScripts.applications.text = let
+  	env = pkgs.buildEnv {
+    	name = "system-applications";
+    	paths = config.environment.systemPackages;
+    	pathsToLink = "/Applications";
+  	};
+      in
+  	pkgs.lib.mkForce ''
+  	# Set up applications.
+  		echo "setting up /Applications..." >&2
+  		rm -rf /Applications/Nix\ Apps
+  		mkdir -p /Applications/Nix\ Apps
+  		find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+  		while read -r src; do
+    			app_name=$(basename "$src")
+    			echo "copying $src" >&2
+    			${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+  		done
+      	'';
+
+      system.defaults = {
+	dock.autohide = true;
+	finder.FXPreferredViewStyle = "clmv";
+      };
 
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
-
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
+      programs.zsh.enable = true;
+      nix.enable = false;
 
       # Set Git commit hash for darwin-version.
       system.configurationRevision = self.rev or self.dirtyRev or null;
-
+      
       # Used for backwards compatibility, please read the changelog before changing.
       # $ darwin-rebuild changelog
-      system.stateVersion = 6;
+      system.stateVersion = 5;
 
       # The platform the configuration will be used on.
       nixpkgs.hostPlatform = "aarch64-darwin";
@@ -52,9 +83,21 @@
   in
   {
     # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#jeff-mbp
-    darwinConfigurations."jeff-mbp" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
+    # $ darwin-rebuild build --flake .#WF11090
+    darwinConfigurations."WF11090" = nix-darwin.lib.darwinSystem {
+      system = system;
+      modules = [ configuration
+      home-manager.darwinModules.home-manager
+	{
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            # home-manager.users.jdoe = ./home.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass
+            # arguments to home.nix
+          }
+
+      ];
     };
   };
 }
