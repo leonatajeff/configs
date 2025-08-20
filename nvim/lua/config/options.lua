569042db129dfe15f95d2725b05f08d8a1c2bef8
leonatajeff <jeffleonata@gmail.com>
2025-08-19 23:08:15 -0400

    add old config

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,9 @@
+-- General options
+vim.opt.relativenumber = true
+vim.opt.scrolloff = 8
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.smartindent = true
+
+-- Colorscheme
+vim.cmd.colorscheme("desert")
